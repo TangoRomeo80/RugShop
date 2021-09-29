2987141ba013ca71eef640ae80829a94bfbb27dd
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-09-29 17:59:37 +0600

    added product actions redux

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,25 +5,23 @@ import {
   PRODUCT_LIST_FAIL,
 } from '../constants/productConstants'
 
-export const listProducts =
-  (keyword = '', pageNumber = '') =>
-  async (dispatch) => {
-    try {
-      dispatch({ type: PRODUCT_LIST_REQUEST })
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_LIST_REQUEST })
 
-      const { data } = await axios.get('api/products')
+    const { data } = await axios.get('api/products')
 
-      dispatch({
-        type: PRODUCT_LIST_SUCCESS,
-        payload: data,
-      })
-    } catch (error) {
-      dispatch({
-        type: PRODUCT_LIST_FAIL,
-        payload:
-          error.response && error.response.data.message
-            ? error.response.data.message
-            : error.message,
-      })
-    }
+    dispatch({
+      type: PRODUCT_LIST_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
   }
+}
