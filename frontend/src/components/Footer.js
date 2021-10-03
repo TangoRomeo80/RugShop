e7498ab1215d2f0a6c03d89879a1194023c30f0a
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-10-03 20:58:41 +0600

    added footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,10 +4,22 @@ import { Container, Row, Col } from 'react-bootstrap'
 const Footer = () => {
   return (
     <footer>
-      <Container>
+      <Container fluid className='bg-dark'>
         <Row>
-          <Col className='text-center py-3'>
-            Copyright &copy; KSM Bedding and Rugs
+          <Col className='text-center py-3 contact-footer'>
+            <h4>Contact Info</h4>
+            <ul>
+              <li>
+                <i className='fa fa-phone'></i>+44 7877 980930
+              </li>
+              <li>
+                <i className='fa fa-envelope-o'></i>abdulhay2014@hotmail.com
+              </li>
+              <li>
+                <i class='fa fa-flag'></i>Unit: 3 and 4, Eastham Market, Pilsgrims way, London,
+                United Kingdom
+              </li>
+            </ul>
           </Col>
         </Row>
       </Container>
