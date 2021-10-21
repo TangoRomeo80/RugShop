da86f91e21a077698e569564b942ad1e8cfda59d
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-10-22 01:19:11 +0600

    changed product file

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
 //import Rating from './Rating'
 
 const Product = ({ product }) => {
+  //const dispatch = useDispatch()
+
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
+
   return (
     <div>
       <Card className='my-3 p-3 rounded'>
@@ -23,12 +31,15 @@ const Product = ({ product }) => {
             /> */}
             In stock: {product.countInStock}
           </Card.Text>
-          <Card.Text as='div'>
-            Size: {product.size}
-          </Card.Text>
-          <Card.Text as='div'>
-            Color: {product.color}
-          </Card.Text>
+          <Card.Text as='div'>Size: {product.size}</Card.Text>
+          <Card.Text as='div'>Color: {product.color}</Card.Text>
+          {userInfo && userInfo.isAdmin && (
+            <LinkContainer to={`/admin/product/${product._id}/edit`}>
+              <Button variant='light' className='btn-sm'>
+                <i className='fas fa-edit'></i>Edit
+              </Button>
+            </LinkContainer>
+          )}
           <Card.Text as='h3'>£{product.price}</Card.Text>
         </Card.Body>
       </Card>
